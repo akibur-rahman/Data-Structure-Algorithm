8cea84271585bce0897bcbb1ea66cc716c003b95
Akibur Rahman <work.akiburrahman@gmail.com>
2022-06-21 07:38:39 +0600

    Edited Linear Search

--- Theory/LinearSearch.c
+++ Theory/LinearSearch.c
@@ -3,15 +3,19 @@
 
 void main()
 {
-    int arr[SZ] = {8, 12, 5, 56, 35, 353, 565, 52, 42, 55, 686}, n;
+    int arr[SZ] = {8, 12, 5, 56, 35, 353, 565, 52, 42, 55, 686}, n, i;
     printf("Enter the element you want:");
     scanf("%d", &n);
-    for (int i = 0; i < SZ; i++)
+    for (i = 0; i < SZ; i++)
     {
         if (arr[i] == n)
         {
             printf("%d is the %dth element", n, i + 1);
             break;
         }
+    }
+    if (i == SZ)
+    {
+        printf("Item not found.");
     }
 }
