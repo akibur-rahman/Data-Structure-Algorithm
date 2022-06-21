ed702e5629fa1e6f52ab53b18213d0623d6f8235
Akibur Rahman <work.akiburrahman@gmail.com>
2022-06-21 09:09:28 +0600

    Added Binary Search and Bubble Sort

---
+++ Theory/BinarySearch.c
@@ -0,0 +1,25 @@
+// This Algorithm needs a sorted array to work.
+
+#include <stdio.h>
+#define SZ 10
+
+void main()
+{
+    int array[SZ] = {2, 4, 6, 8, 9, 12, 36, 54, 65, 78}, n, i;
+    printf("Enter the element you want:");
+    scanf("%d", &n);
+    int low = 0, high = SZ - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (array[mid] == n)
+            printf("%d is %dth element\n", n, mid + 1);
+
+        if (array[mid] < n)
+            low = mid + 1;
+
+        else
+            high = mid - 1;
+    }
+}
--- Theory/bubbleSort.c
+++ Theory/bubbleSort.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#define SZ 10
 
 void main()
 {
-    int arr[10] = {8, 23, 45, 4, 34, 56, 6, 46, 7, 2};
+    int arr[SZ] = {8, 23, 45, 4, 34, 56, 6, 46, 7, 2}, i, j, temp, flag;
+
+    for (i = 0; i < SZ - 1; i++)
+    {
+        flag = 0;
+        for (j = 0; j < SZ - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                flag = 1;
+            }
+        }
+        if (flag == 0)
+        {
+            break;
+        }
+    }
+
+    for (i = 0; i < SZ; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 }
