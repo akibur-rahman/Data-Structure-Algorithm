da353b99f257cbee66b83e7e2675320cf4797a1b
Akibur Rahman <me.akiburrahman@gmail.com>
2022-08-12 11:19:56 +0600

    created project

---
+++ Project/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct node
+{
+    char name[20];
+    char number[10];
+} node;
+
+void selectionment()
+{
+    int choice;
+    printf("Enter Your Choice:");
+    scanf("%d", &choice);
+}
+
+void main()
+{
+    selectionmen();
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Data-Structure
- DS theory and practices 
