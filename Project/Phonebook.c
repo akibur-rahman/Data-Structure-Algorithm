f947f2f63345fad8f81296574397acafa2e333f4
Akibur Rahman <me.akiburrahman@gmail.com>
2022-08-13 10:07:53 +0600

    Created phonebook

--- Project/Phonebook.c
+++ Project/Phonebook.c
@@ -4,16 +4,26 @@ typedef struct node
 {
     char name[20];
     char number[10];
+
 } node;
 
-void selectionment()
+void selectionmenu()
 {
     int choice;
     printf("Enter Your Choice:");
     scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        break;
+
+    default:
+        break;
+    }
 }
 
 void main()
 {
-    selectionmen();
+    selectionmenu();
 }
