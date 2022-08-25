7ba825c2c8b358009f40c3dab5464f7cec9cfc55
Akibur Rahman <me.akiburrahman@gmail.com>
2022-08-25 23:01:07 +0600

    added print function

--- Project/Phonebook.c
+++ Project/Phonebook.c
@@ -4,8 +4,21 @@ typedef struct node
 {
     char name[20];
     char number[10];
+    struct node *next;
 
 } node;
+
+node *head, *temp, *prev, *next;
+
+void printlist()
+{
+    temp = head;
+    while (temp != NULL)
+    {
+        printf("%s\t%d", temp->name, temp->number);
+        temp = temp->next;
+    }
+}
 
 void selectionmenu()
 {
