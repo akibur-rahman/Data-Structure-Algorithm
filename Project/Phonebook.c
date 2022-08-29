43bfe7ca3af5ce32bf9193033550c6d126384552
Akibur Rahman <me.akiburrahman@gmail.com>
2022-08-29 20:22:04 +0600

    added file read

--- Project/Phonebook.c
+++ Project/Phonebook.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct node
 {
@@ -10,12 +12,48 @@ typedef struct node
 
 node *head, *temp, *prev, *next;
 
+void read_data()
+{
+    head = NULL, prev = NULL;
+    FILE *fp = fopen("data.txt", "r");
+    if (fp == NULL)
+    {
+        printf("Unable to Read data from file. Make sure data.txt is in the same directory\a\n");
+        exit(1);
+    }
+    char line[50];
+    char *sp;
+    while (fgets(line, 50, fp) != NULL)
+    {
+        temp = (node *)malloc(sizeof(node));
+        sp = strtok(line, ",");
+        strcpy(temp->name, sp);
+        sp = strtok(NULL, ",");
+        strcpy(temp->number, sp);
+        temp->next == NULL;
+        if (head == NULL)
+        {
+            temp->next = NULL;
+            head = temp;
+            prev = head;
+        }
+        else
+        {
+            prev->next = temp;
+            temp->next = NULL;
+            prev = prev->next;
+        }
+    }
+
+    fclose(fp);
+}
+
 void printlist()
 {
     temp = head;
     while (temp != NULL)
     {
-        printf("%s\t%d", temp->name, temp->number);
+        printf("%s\t%s\n", temp->name, temp->number);
         temp = temp->next;
     }
 }
@@ -38,5 +76,6 @@ void selectionmenu()
 
 void main()
 {
-    selectionmenu();
+    read_data();
+    printlist();
 }
